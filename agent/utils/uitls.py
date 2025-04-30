811ebfc7669257919785694b9b36d40d5766dfd5
Yong Hu (FESCO Adecco Human Resources) <v-huyon@microsoft.com>
2025-04-30 11:03:23 +0800

    Code Review

--- agent/utils/uitls.py
+++ agent/utils/uitls.py
@@ -1,4 +1,8 @@
+import aiohttp
+import ssl
+import certifi
 import random
+
 
 
 async def get_first_photo_url_async(session, keywords, key):
@@ -8,18 +12,20 @@ async def get_first_photo_url_async(session, keywords, key):
         "keywords": keywords,
         "show_fields": "photos"
     }
+
+    ssl_context = ssl.create_default_context(cafile=certifi.where())
     
     try:
-        async with session.get(url, params=params, timeout=40) as response:
-            data = await response.json()
-
-            if data.get("status") == "1" and data.get("pois"):
-                poi = random.choice(data.get("pois"))
-                photos = poi.get("photos", [])
-                if photos:
-                    photo = random.choice(photos)
-                    return photo.get("url", '')
-            return ''
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=ssl_context)) as session:
+            async with session.get(url, params=params, timeout=40) as response:
+                data = await response.json()
+                if data.get("status") == "1" and data.get("pois"):
+                    poi = random.choice(data.get("pois"))
+                    photos = poi.get("photos", [])
+                    if photos:
+                        photo = random.choice(photos)
+                        return photo.get("url", '')
+                return ''
     except Exception as e:
         print(f"获取图片出错了：{e}")
         return ''
